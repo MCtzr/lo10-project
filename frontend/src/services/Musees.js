a79d1d5ba4a8b2c3f9cff048d3d899a312f2ab87
Liliane <lilianeli2000@gmail.com>
2023-05-14 18:01:11 +0200

    Barre de recherche

--- frontend/src/services/Musees.js
+++ frontend/src/services/Musees.js
@@ -1,51 +1,55 @@
-import data from './liste-musees.json';
 import './Musees.css';
 import React, { useState, useEffect } from 'react';
 
-function Musees() {
-  //const listemusees = data;
-  const [listemusees, setList] = useState(data);
-  const [visibleCount, setVisibleCount] = useState(10);
-  const [loading, setLoading] = useState(false);
+function Musees(props) {
+  //useEffect(() => {
+  //  setList(props.data.slice(0, visibleCount));
+  //}, [visibleCount]);
 
-  useEffect(() => {
-    setList(data.slice(0, visibleCount));
-  }, [visibleCount]);
+  //function handleScroll() {
+  //  const windowHeight =
+  //     'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
+  //   const body = document.body;
+  //   const html = document.documentElement;
+  //   const docHeight = Math.max(
+  //     body.scrollHeight,
+  //     body.offsetHeight,
+  //     html.clientHeight,
+  //     html.scrollHeight,
+  //     html.offsetHeight
+  //   );
+  //   const windowBottom = windowHeight + window.pageYOffset;
+  //   if (windowBottom >= docHeight && !loading) {
+  //     setLoading(true);
+  //     setVisibleCount((prevCount) => prevCount + 10);
+  //     setTimeout(() => setLoading(false), 1000);
+  //   }
+  // }
 
-  function handleScroll() {
-    const windowHeight =
-      'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
-    const body = document.body;
-    const html = document.documentElement;
-    const docHeight = Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    );
-    const windowBottom = windowHeight + window.pageYOffset;
-    if (windowBottom >= docHeight && !loading) {
-      setLoading(true);
-      setVisibleCount((prevCount) => prevCount + 10);
-      setTimeout(() => setLoading(false), 1000);
-    }
-  }
+  const imageExists = (url) => {
+    const img = new Image();
+    img.src = url;
+    return img.complete && img.naturalHeight !== 0;
+  };
 
   return (
-    <div onScroll={handleScroll}>
-      
-      {listemusees.map((musees) => (
-        <div className="divMusees" key={musees.identifiant_museofile}>
-          <img src={`https://s3.eu-west-3.amazonaws.com/pop-phototeque/museo/${musees.identifiant_museofile}/museofile${musees.identifiant_museofile}.jpg`} alt={`${musees.identifiant_museofile}`}></img>
-          <h2>{musees.nom_officiel_du_musee}</h2>
-          <p>{musees.adresse}</p>
-          <p className= "adresse">{musees.code_postal} {musees.commune}</p>
-          <p className="tel"><i class="fa-solid fa-phone"></i> Téléphone : {musees.telephone}</p>
-          <p className="url">Site web : <a href={musees.url}>{musees.url}</a></p>
-        </div>
-      ))}
-      {loading && <div>Loading...</div>}
+    <div>
+      {props.data.map((musee) => {
+        const imageUrl = `https://s3.eu-west-3.amazonaws.com/pop-phototeque/museo/${musee.identifiant_museofile}/museofile${musee.identifiant_museofile}.jpg`;
+        if (imageExists(imageUrl)) {
+          return (
+            <div className="musees" key={musee.identifiant_museofile}>
+              <img src={imageUrl} alt={musee.identifiant_museofile}></img>
+              <h2>{musee.nom_officiel_du_musee}</h2>
+              <p>{musee.adresse}</p>
+              <p className="adresse">{musee.code_postal} {musee.commune}</p>
+              <p className="tel"><i className="fa-solid fa-phone"></i> Téléphone : {musee.telephone}</p>
+              <p className="url">Site web : <a href={musee.url}>{musee.url}</a></p>
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
--- frontend/src/views/page1.js
+++ frontend/src/views/page1.js
@@ -1,13 +1,43 @@
-import Musees from '../services/Musees';
 import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import Musees from '../services/Musees';
+import Searchbar from './Searchbar';
+import data from '../services/liste-musees.json';
 
 function Page1() {
 
     const { userId } = useParams();
+    const [musees, setMusee] = useState(data)
+    const [query, setQuery] = useState([])
+
+    const searchMusee = () => {
+        const filteredResults = data.filter((item) => {
+          const museeName = item.nom_officiel_du_musee;
+          return museeName && museeName.toLowerCase().includes(query?.toLowerCase?.());
+        });
+      
+        setMusee(filteredResults);
+    };
+
+    useEffect(() => {
+        searchMusee();
+    }, [query]);
+
+    // const searchMusee = async () => {
+    //     setQuery(query);
+
+    //     const filteredResults = data.filter((item) => {
+    //         const museeName = item.nom_officiel_du_musee;
+    //         return museeName && museeName.toLowerCase().includes(query.toLowerCase());
+    //     });
+
+    //     setMusee([filteredResults]);
+    // };
 
     return (
         <>
-            <Musees />
+            <Searchbar search={searchMusee} searchQuery={query} onWriting={setQuery}/>
+            <Musees data={musees}/>
         </>
     )
 }
